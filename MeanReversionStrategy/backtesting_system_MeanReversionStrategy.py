555003e044ba4d4b1f60d108519bcc4532d0abfe
akiraawan <akiraalexanderwan@gmail.com>
2023-11-17 18:33:19 +0000

    changes

--- MeanReversionStrategy/backtesting_system_MeanReversionStrategy.py
+++ MeanReversionStrategy/backtesting_system_MeanReversionStrategy.py
@@ -1,14 +1,14 @@
 import yfinance as yf
 import numpy as np
 import pandas as pd
-import tensorflow as tf
-import csv
 import matplotlib.pyplot as plt
 plt.style.use("seaborn")
-'''
+"""
 Object-Oriented Backtesting System for Mean Reversion Strategy
 
-'''
+"""
+
+
 class Backtest:
     def __init__(self, symbol_file, yahoo=True):
         if yahoo:
@@ -19,7 +19,7 @@ class Backtest:
             file = symbol_file
             self.df = pd.read_csv(file, names=["Date", "Price"], header=None, index_col=0, dayfirst=True)
             self.df.index = pd.to_datetime(self.df.index, dayfirst=True)
-        
+
         if self.df.empty:
             print("No data pulled")
         else:
@@ -32,8 +32,8 @@ class Backtest:
         self.df["Lower_bb"] = self.df.SMA - self.df.Price.rolling(SMA).std() * devs[0]
         self.df["Upper_bb"] = self.df.SMA + self.df.Price.rolling(SMA).std() * devs[1]
         self.df["distance"] = self.df.Price - self.df.SMA
-        self.df.dropna(inplace = True)
-    
+        self.df.dropna(inplace=True)
+
     def generate_signals(self):
         self.df["position"] = np.where(self.df.Price < self.df.Lower_bb, 1, np.nan)
         self.df["position"] = np.where(self.df.Price > self.df.Upper_bb, -1, self.df.position)
@@ -43,7 +43,7 @@ class Backtest:
         self.df["Trade_Signal"] = self.df.position - self.df.position.shift(1)
 
     def calc_metric(self, metric="PnL"):
-        if metric=="PnL":
+        if metric == "PnL":
             self.df["PnL"] = np.nan
             current_position = 0
             position_price = 0
@@ -67,30 +67,30 @@ class Backtest:
 
             self.df["cumPnL"] = self.df["PnL"].cumsum()
             metric = self.df["PnL"].sum()
-        
-        elif metric=="log_return":
+
+        elif metric == "log_return":
             self.df["log_returns"] = np.log(self.df.div(self.df.shift(1)))
             self.df["log_strategy"] = self.df.position.shift(1) * self.df["log_returns"]
-            self.df.dropna(inplace = True)
+            self.df.dropna(inplace=True)
 
             metric = np.exp(self.df["log_strategy"].sum())
 
         return metric
 
     def plot(self, start, end=None, cols=None, format='-', with_signal=False):
-        if cols==None:
+        if cols is None:
             cols = ["Price"]
-        ax = self.df[cols].loc[start:end].plot(figsize=(12,8))
+        ax = self.df[cols].loc[start:end].plot(figsize=(12, 8))
         if with_signal:
-            buysignals = self.df[self.df["Trade_Signal"]==1.0].loc[start:end].index
-            sellsignals = self.df[self.df["Trade_Signal"]==-1.0].loc[start:end].index
-            
+            buysignals = self.df[self.df["Trade_Signal"] == 1.0].loc[start:end].index
+            sellsignals = self.df[self.df["Trade_Signal"] == -1.0].loc[start:end].index
+
             plt.plot(buysignals, self.df.loc[buysignals, "Price"], "g^")
             plt.plot(sellsignals, self.df.loc[sellsignals, "Price"], "rv")
         plt.show()
 
     def plot_PnL(self, start, end=None, cumulative=False, format='-'):
         if not cumulative:
-            self.df.dropna()["PnL"].loc[start:end].plot(figsize=(12,8))
+            self.df.dropna()["PnL"].loc[start:end].plot(figsize=(12, 8))
         else:
-            self.df.dropna()["cumPnL"].loc[start:end].plot(figsize=(12,8))
+            self.df.dropna()["cumPnL"].loc[start:end].plot(figsize=(12, 8))
--- MeanReversionStrategy/backtesting_system_MeanReversionStrategy2.py
+++ MeanReversionStrategy/backtesting_system_MeanReversionStrategy2.py
@@ -1,14 +1,14 @@
 import yfinance as yf
 import numpy as np
 import pandas as pd
-import tensorflow as tf
-import csv
 import matplotlib.pyplot as plt
 plt.style.use("seaborn")
-'''
+"""
 Object-Oriented Backtesting System for Mean Reversion Strategy
 
-'''
+"""
+
+
 class Backtest:
     def __init__(self, symbol_file, yahoo=True):
         if yahoo:
@@ -17,9 +17,9 @@ class Backtest:
             self.df.columns = ["Price"]
         else:
             file = symbol_file
-            self.df = pd.read_csv(file, names=["Date", "Price"], header=None, index_col=0, dayfirst=True)
+            self.df = pd.read_csv(file, names=["Date", "Price"], header=None, index_cols=0, dayfirst=True)
             self.df.index = pd.to_datetime(self.df.index, dayfirst=True)
-        
+
         if self.df.empty:
             print("No data pulled")
         else:
@@ -35,19 +35,19 @@ class Backtest:
         self.df["Upper_1sd"] = self.df.SMA + self.df.Price.rolling(SMA).std()
         self.df["UpperDistance"] = self.df.Price - self.df.Upper_1sd
         self.df["LowerDistance"] = self.df.Price - self.df.Lower_1sd
-        self.df.dropna(inplace = True)
-    
+        self.df.dropna(inplace=True)
+
     def generate_signals(self):
         self.df["position"] = np.where(self.df.Price < self.df.Lower, 1, np.nan)
         self.df["position"] = np.where(self.df.Price > self.df.Upper_BB, -1, self.df.position)
-        self.df["position"] = np.where(self.df.UpperDistance* self.df.UpperDistance.shift(1) < 0, 0, self.df.position)
-        self.df["position"] = np.where(self.df.LowerDistance* self.df.LowerDistance.shift(1) < 0, 0, self.df.position)
+        self.df["position"] = np.where(self.df.UpperDistance * self.df.UpperDistance.shift(1) < 0, 0, self.df.position)
+        self.df["position"] = np.where(self.df.LowerDistance * self.df.LowerDistance.shift(1) < 0, 0, self.df.position)
         self.df["position"] = self.df.position.ffill().fillna(0)
         self.df.loc[self.df.index[-1], "position"] = 0
         self.df["Trade_Signal"] = self.df.position - self.df.position.shift(1)
 
     def calc_metric(self, metric="PnL"):
-        if metric=="PnL":
+        if metric == "PnL":
             self.df["PnL"] = np.nan
             current_position = 0
             position_price = 0
@@ -71,30 +71,30 @@ class Backtest:
 
             self.df["cumPnL"] = self.df["PnL"].cumsum()
             metric = self.df["PnL"].sum()
-        
-        elif metric=="log_return":
+
+        elif metric == "log_return":
             self.df["log_returns"] = np.log(self.df.div(self.df.shift(1)))
             self.df["log_strategy"] = self.df.position.shift(1) * self.df["log_returns"]
-            self.df.dropna(inplace = True)
+            self.df.dropna(inplace=True)
 
             metric = np.exp(self.df["log_strategy"].sum())
 
         return metric
 
     def plot(self, start, end=None, cols=None, format='-', with_signal=False):
-        if cols==None:
+        if cols is None:
             cols = ["Price"]
-        ax = self.df[cols].loc[start:end].plot(figsize=(12,8))
+        ax = self.df[cols].loc[start:end].plot(figsize=(12, 8))
         if with_signal:
-            buysignals = self.df[self.df["Trade_Signal"]==1.0].loc[start:end].index
-            sellsignals = self.df[self.df["Trade_Signal"]==-1.0].loc[start:end].index
-            
+            buysignals = self.df[self.df["Trade_Signal"] == 1.0].loc[start:end].index
+            sellsignals = self.df[self.df["Trade_Signal"] == -1.0].loc[start:end].index
+
             plt.plot(buysignals, self.df.loc[buysignals, "Price"], "g^")
             plt.plot(sellsignals, self.df.loc[sellsignals, "Price"], "rv")
         plt.show()
 
     def plot_PnL(self, start, end=None, cumulative=False, format='-'):
         if not cumulative:
-            self.df.dropna()["PnL"].loc[start:end].plot(figsize=(12,8))
+            self.df.dropna()["PnL"].loc[start:end].plot(figsize=(12, 8))
         else:
-            self.df.dropna()["cumPnL"].loc[start:end].plot(figsize=(12,8))
+            self.df.dropna()["cumPnL"].loc[start:end].plot(figsize=(12, 8))
